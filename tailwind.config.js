3ec30097bf2ad7127e5d6926942aadca5a4d0bb7
poieticid <ypsiron@gmail.com>
2025-06-13 15:10:46 +0900

    commit poietic.site

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,9 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,ts,jsx,tsx,mdx}"
+    './src/**/*.{js,ts,jsx,tsx}', // 必ずsrc配下を含めてください
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
