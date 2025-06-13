f84131cda6a5e1fae3a9a7eb054632ec9608fece
poieticid <ypsiron@gmail.com>
2025-06-14 00:35:59 +0900

    commit poietic.site

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  plugins: ['prettier-plugin-tailwindcss'],
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'es5',
+  tabWidth: 2,
+  printWidth: 100,
+}
