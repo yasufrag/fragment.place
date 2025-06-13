fd406e5a3820e8845d62fb137ca73578dd198699
poieticid <ypsiron@gmail.com>
2025-06-13 15:52:59 +0900

    commit poietic.site

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,9 +1,8 @@
-// src/components/Layout.tsx
 import { ReactNode } from 'react'
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <div className="min-h-screen px-4 py-8">
+    <div className="min-h-screen px-6 py-12 max-w-3xl mx-auto">
       {children}
     </div>
   )
