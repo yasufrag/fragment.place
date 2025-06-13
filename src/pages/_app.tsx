ea4b99639c30579c309e1c856438b65152fe178a
poieticid <ypsiron@gmail.com>
2025-06-13 14:38:59 +0900

    commit poietic.site

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,8 @@
 import '../styles/globals.css';
+import type { AppProps } from 'next/app';
 import Header from '../components/Header';
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Header />
