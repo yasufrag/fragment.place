3f4d5c18767d9abbaf6c3a26371f80dd2ca1bbb4
poieticid <ypsiron@gmail.com>
2025-06-14 19:56:06 +0900

    commit poietic.site

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import { meta } from '@/meta/meta'
 export default function Index() {
   return (
     <>
-      <MetaTag {...meta.home} />
+      <MetaTag {...meta.index} />
       <h1>co.poiesis</h1>
       <p>a frame for poetic fragments and living syntax</p>
     </>
