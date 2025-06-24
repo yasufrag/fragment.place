9947976ada8e9cd2689aa503e04612f5274f724c
poieticid <ypsiron@gmail.com>
2025-06-24 19:45:16 +0900

    commit poietic.site

--- src/app/zines/page.tsx
+++ src/app/zines/page.tsx
@@ -23,7 +23,7 @@ export default function ZinesPage() {
 
   return (
     <>
-      <MetaTag {...meta.zines} />
+      <MetaTag {...meta.zines.index} />
 
       <div className="max-w-4xl mx-auto px-6 py-12 pb-24">
         <h1 className="text-3xl font-bold mb-4">ZINEs</h1>
