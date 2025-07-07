66a2d8af5a4df62b127fe7532622a94a164f801b
poieticid <ypsiron@gmail.com>
2025-07-07 22:52:27 +0900

    commit poietic.site

--- src/components/ArticleButton.tsx
+++ src/components/ArticleButton.tsx
@@ -30,12 +30,12 @@ export default function FragmentFooter() {
         </a>
       </div>
 
-      <div className="flex gap-3">
+      <div className="flex flex-col sm:flex-row sm:gap-3 gap-2 w-full sm:w-auto mt-2 sm:mt-0">
         <a
           href="https://www.buymeacoffee.com/poieticid"
           target="_blank"
           rel="noopener noreferrer"
-          className="inline-block bg-yellow-500 text-stone-800 px-5 py-2 rounded duration-200 hover:bg-yellow-600 hover:text-stone-800"
+          className="w-full sm:w-auto text-center bg-yellow-500 text-stone-800 px-5 py-2 rounded duration-200 hover:bg-yellow-600 hover:text-stone-800"
         >
           Support poetic writing
         </a>
@@ -43,7 +43,7 @@ export default function FragmentFooter() {
           href="https://ofuse.me/poieticid"
           target="_blank"
           rel="noopener noreferrer"
-          className="inline-block bg-blue-500 text-white px-5 py-2 rounded duration-200 hover:bg-blue-600"
+          className="w-full sm:w-auto text-center bg-blue-500 text-white px-5 py-2 rounded duration-200 hover:bg-blue-600"
         >
           OFUSEで支援する
         </a>
